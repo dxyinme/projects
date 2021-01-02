795c7769680a57e0955859e6bdc0de0152a19735
xiyaodong <xiyaodong@tencent.com>
2021-01-02 15:28:51 +0800

    daytime suffix

--- examples/dlogger_example/example.cpp
+++ examples/dlogger_example/example.cpp
@@ -1,10 +1,10 @@
 #include "util/dlogger/dlogger.h"
 
 int main() {
-    dlogger::init_log("xxxx.log", true);
-    DLOG(dlogger::INFO, 1,2,3,4,"ccc", "INFO");
-    DLOGF(dlogger::DEBUG, "t1=%d,t2=%d,s1=%s", 108, 1, "debug");
-    DLOGF(dlogger::WARN, "t1=%d,t2=%d,s1=%s", 101, 103, "warn");
-    DLOGF(dlogger::ERROR, "t1=%d,t2=%d,s1=%s", 2333, 1023, "error");
+    dlogger::init_log("xxxx.log", false);
+    for(int i=1;i<=(1<<10);i++) {
+        DLOGF(dlogger::WARN, "now i = %d : we are test\ncases", i);
+    }
+    dlogger::log_flush();
     return 0;
 }
