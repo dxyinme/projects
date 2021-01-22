56089b6857f0233816676076b2aa0552ea26a396
dxyinme <dxyinme@outlook.com>
2021-01-22 11:05:51 +0800

    update md5

--- util/md5/md5.h
+++ util/md5/md5.h
@@ -3,9 +3,24 @@
 namespace util{
 namespace md5{
 
-void md5(const uint8_t* initial_msg, size_t initial_len, uint8_t* digest);
+void md5_string(char* msg, uint8_t* result);
 
-void md5_string(char* msg, uint8_t* digest);
+
+// prepare for large file.
+class md5_worker {
+private:
+    uint8_t* digest;
+    uint32_t h0, h1, h2, h3;
+    size_t current_size, new_size;
+    bool isend;
+public:
+    // sum_size_ = len(`string`) % pow(2, 64)
+    md5_worker();
+    // confirm now_len <= 512
+    void update(const uint8_t* now_msg, size_t now_len, bool is_end);
+    void get_digest(uint8_t* result);
+    ~md5_worker();
+};
 
 };
 };
--- util/md5/md5_sum.cpp
+++ util/md5/md5_sum.cpp
@@ -3,19 +3,38 @@
 #include <stdint.h>
 #include <cstdio>
 
+// can be used in big file md5 calculating
+
+int min(size_t A, size_t B) {return A < B ? A : B;}
+
 int main(int argc, char* argv[]) {
     if (argc < 1) {
         return 0;
     }
     FILE* f = fopen(argv[1], "rb");
-    fseek(f, 0L, SEEK_END);
-    int64_t sz = ftell(f);
-    char* fcon = new char[sz + 5];
-    fseek(f, 0L, SEEK_SET);
-    fread(fcon, sz, 1, f);
-    fclose(f);
+    fseek(f, 0LL, SEEK_END);
+    size_t sz = ftell(f);
+    char* fcon = new char[(512 / 8) * 2 + 5];
+    fseek(f, 0LL, SEEK_SET);
     uint8_t result[16];
-    util::md5::md5_string(fcon, result);
+    size_t offset = 0;
+
+    util::md5::md5_worker md5Worker;
+    while(offset < sz) {
+        size_t now_read_size = min((size_t) 512 / 8, sz - offset);
+        fread(fcon, now_read_size, 1, f);
+        if(offset + (512 / 8) >= sz) { // end of string
+            md5Worker.update((uint8_t *) fcon, now_read_size, true);
+            break;
+        } else {
+            md5Worker.update((uint8_t *) fcon, now_read_size, false);
+        }
+        offset += now_read_size;
+    }
+
+    fclose(f);
+    
+    md5Worker.get_digest(result);
     for(int i=0;i<16;i++) printf("%2.2x", result[i]);
     printf(" %s\n", argv[1]);
     return 0;
